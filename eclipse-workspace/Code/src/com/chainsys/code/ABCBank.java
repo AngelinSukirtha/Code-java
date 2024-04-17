87b1ffe677d452d20f9b13ff19cf887c77e996a4
ange3577 <ange3577@CSL0692.global.chainsys.com>
2024-04-17 11:06:21 +0530

    Encapsulation,Overloading

---
+++ eclipse-workspace/Code/src/com/chainsys/code/ABCBank.java
@@ -0,0 +1,5 @@
+package com.chainsys.code;
+
+public class ABCBank {
+
+}
---
+++ eclipse-workspace/Code/src/com/chainsys/code/TestHospital.java
@@ -0,0 +1,10 @@
+package com.chainsys.code;
+
+public class TestHospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
