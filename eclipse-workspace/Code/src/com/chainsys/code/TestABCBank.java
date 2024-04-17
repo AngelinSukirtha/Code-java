97c8192d7192a4876986511879bd151c777ffcd3
ange3577 <ange3577@CSL0692.global.chainsys.com>
2024-04-17 14:43:47 +0530

    Overloading

--- eclipse-workspace/Code/src/com/chainsys/code/TestABCBank.java
+++ eclipse-workspace/Code/src/com/chainsys/code/TestABCBank.java
@@ -4,7 +4,9 @@ public class TestABCBank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ABCBank abcbank = new ABCBank();
+		abcbank.getUserDetails();
+		abcbank.printUserDetails();
+		abcbank.deposit(4000, 5432675, "Angelin");
 	}
-
 }
