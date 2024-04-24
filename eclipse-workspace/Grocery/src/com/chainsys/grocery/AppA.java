d1cec9c0a26ef731b3a8bb5eb8d2a2826d35dc57
ange3577 <ange3577@CSL0692.global.chainsys.com>
2024-04-24 14:29:29 +0530

    Projects

--- eclipse-workspace/Grocery/src/com/chainsys/grocery/AppA.java
+++ eclipse-workspace/Grocery/src/com/chainsys/grocery/AppA.java
@@ -19,7 +19,8 @@ public class AppA {
 
 	public void categoryDetails() {
 		System.out.println(" ");
-		System.out.println("IN GROCER'S CHOICE WE HAVE"+ "\n1.Fruits"+"\n2.Vegetables"+"\n3.Nuts"+"\n4.Stationery"+"\n5.Drinks"+"\n6.Grains");
+		System.out.println("IN GROCER'S CHOICE WE HAVE" + "\n1.Fruits" + "\n2.Vegetables" + "\n3.Nuts"
+				+ "\n4.Stationery" + "\n5.Drinks" + "\n6.Grains");
 		System.out.println("CHOOSE YOUR CATEGORY FROM ABOVE");
 		Scanner sc = new Scanner(System.in);
 		String categoryInput = sc.nextLine();
@@ -39,45 +40,8 @@ public class AppA {
 			System.out.println("AVAILABLE ITEMS");
 			System.out.println(
 					"1.Apple" + "\n2.Orange" + "\n3.Grapes" + "\n4.Banana" + "\n5.Watermelon" + "\n6.PineApple");
-			System.out.println("Enter the item you want:");
-			String itemInput = sc.nextLine();
-			switch (itemInput) {
-			case "Apple":
-				System.out.println("You chose Apple from the Fruits category.");
-				price=10;
-				System.out.println("Price is "+price);
-				break;
-			case "Orange":
-				System.out.println("You chose orange from the Fruits category.");
-				price=12;
-				System.out.println("Price is "+price);
-				break;
-			case "Grapes":
-				System.out.println("You chose grapes from the Fruits category.");
-				price=15;
-				System.out.println("Price is "+price);
-				break;
-			case "Banana":
-				System.out.println("You chose banana from the Fruits category.");
-				price=6;
-				System.out.println("Price is "+price);
-				break;
-			case "Watermelon":
-				System.out.println("You chose Watermelon from the Fruits category.");
-				price=30;
-				System.out.println("Price is "+price);
-				break;
-			case "Pineapple":
-				System.out.println("You chose pineapple from the Fruits category.");
-				price=40;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput = sc.nextLine();
-				break;
-			}
+
+			itemInput();
 			break;
 		case "Vegetables":
 			System.out.println(" ");
@@ -85,250 +49,329 @@ public class AppA {
 			System.out
 					.println("1.Potato" + "\n2.Tomato" + "\n3.Onion" + "\n4.Carrot" + "\n5.Beetroot" + "\n6.Broccoli");
 			System.out.println("Enter the item you want:");
-			String itemInput1 = sc.nextLine();
-
-			switch (itemInput1) {
-			case "Potato":
-				System.out.println("You chose potato from the Vegetables category.");
-				price=10;
-				System.out.println("Price is "+price);
-				break;
-			case "Tomato":
-				System.out.println("You chose tomato from the Vegetables category.");
-				price=5;
-				System.out.println("Price is "+price);
-				break;
-			case "Onion":
-				System.out.println("You chose onion from the Vegetables category.");
-				price=6;
-				System.out.println("Price is "+price);
-				break;
-			case "Carrot":
-				System.out.println("You chose carrot from the Vegetables category.");
-				price=8;
-				System.out.println("Price is "+price);
-				break;
-			case "Beetroot":
-				System.out.println("You chose beetroot from the Vegetables category.");
-				price=10;
-				System.out.println("Price is "+price);
-				break;
-			case "Broccoli":
-				System.out.println("You chose broccoli from the Vegetables category.");
-				price=12;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput1 = sc.nextLine();
-				break;
-			}
+			itemInput1();
 			break;
 		case "Nuts":
 			System.out.println(" ");
 			System.out.println("AVAILABLE ITEMS");
 			System.out.println(
 					"1.Almonds" + "\n2.Walnuts" + "\n3.Pecans" + "\n4.Cashews" + "\n5.Hazelnuts" + "\n6.Peanuts");
-			System.out.println("Enter the item you want:");
-			String itemInput2 = sc.nextLine();
+			itemInput2();
 
-			switch (itemInput2) {
-			case "Almonds":
-				System.out.println("You chose almonds from the Nuts category.");
-				price=4;
-				System.out.println("Price is "+price);
-				break;
-			case "Walnuts":
-				System.out.println("You chose walnuts from the Nuts category.");
-				price=5;
-				System.out.println("Price is "+price);
-				break;
-			case "Pecans":
-				System.out.println("You chose pecans from the Nuts category.");
-				price=3;
-				System.out.println("Price is "+price);
-				break;
-			case "Cashews":
-				System.out.println("You chose cashews from the Nuts category.");
-				price=8;
-				System.out.println("Price is "+price);
-				break;
-			case "Hazelnuts":
-				System.out.println("You chose hazelnut from the Nuts category.");
-				price=10;
-				System.out.println("Price is "+price);
-				break;
-			case "Peanuts":
-				System.out.println("You chose peanuts from the Nuts category.");
-				price=3;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput2 = sc.nextLine();
-				break;
-			}
 			break;
+
 		case "Stationery":
 			System.out.println("AVAILABLE ITEMS");
 			System.out.println("1.Notebooks" + "\n2.Diaries" + "\n3.Pen" + "\n4.Pencils" + "\n5.Calculator"
 					+ "\n6.Punching Machine");
-			System.out.println("Enter the item you want:");
+			itemInput3();
 
-			String itemInput3 = sc.nextLine();
-
-			switch (itemInput3) {
-			case "Notebooks":
-				System.out.println("You chose notebooks from the Stationery category.");
-				price=50;
-				System.out.println("Price is "+price);
-				break;
-			case "Diaries":
-				System.out.println("You chose diaries from the Stationery category.");
-				price=200;
-				System.out.println("Price is "+price);
-				break;
-			case "Pen":
-				System.out.println("You chose pen from the Stationery category.");
-				price=20;
-				System.out.println("Price is "+price);
-				break;
-			case "Pencils":
-				System.out.println("You chose pencils from the Stationery category.");
-				price=5;
-				System.out.println("Price is "+price);
-				break;
-			case "Calculator":
-				System.out.println("You chose calculator from the Stationery category.");
-				price=500;
-				System.out.println("Price is "+price);
-				break;
-			case "Punching Machine":
-				System.out.println("You chose punching machine from the Stationery category.");
-				price=600;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput3 = sc.nextLine();
-				break;
-			}
 			break;
+
 		case "Drinks":
 			System.out.println(" ");
 			System.out.println("AVAILABLE ITEMS");
 			System.out
 					.println("1.Coca-Cola" + "\n2.Pepsi" + "\n3.Limca" + "\n4.Sprite" + "\n5.Fanta" + "\n6.Appy Fizz");
-			System.out.println("Enter the item you want:");
-			String itemInput4 = sc.nextLine();
+			itemInput4();
 
-			switch (itemInput4) {
-			case "Coca-Cola":
-				System.out.println("You chose Coca-Cola from the Drinks category.");
-				price=25;
-				System.out.println("Price is "+price);
-				break;
-			case "Pepsi":
-				System.out.println("You chose Pepsi from the Drinks category.");
-				price=30;
-				System.out.println("Price is "+price);
-				break;
-			case "Limca":
-				System.out.println("You chose Limca from the Drinks category.");
-				price=15;
-				System.out.println("Price is "+price);
-				break;
-			case "Sprite":
-				System.out.println("You chose Sprite from the Drinks category.");
-				price=35;
-				System.out.println("Price is "+price);
-				break;
-			case "Fanta":
-				System.out.println("You chose Fanta from the Drinks category.");
-				price=25;
-				System.out.println("Price is "+price);
-				break;
-			case "Appy Fizz":
-				System.out.println("You chose Appy Fizz from the Drinks category.");
-				price=30;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput4 = sc.nextLine();
-				break;
-			}
 			break;
 		case "Grains":
 			System.out.println(" ");
 			System.out.println("AVAILABLE ITEMS");
 			System.out.println(
 					"1.Wheat" + "\n2.Oats" + "\n3.Rice" + "\n4.Corn" + "\n5.Barley" + "\n6.Sorghum" + "\n7.Millet");
-			System.out.println("Enter the item you want:");
-			String itemInput5 = sc.nextLine();
-
-			switch (itemInput5) {
-			case "Wheat":
-				System.out.println("You chose wheat from the Grains category.");
-				price=50;
-				System.out.println("Price is "+price);
-				break;
-			case "Oats":
-				System.out.println("You chose oats from the Grains category.");
-				price=50;
-				System.out.println("Price is "+price);
-				break;
-			case "Rice":
-				System.out.println("You chose rice from the Grains category.");
-				price=60;
-				System.out.println("Price is "+price);
-				break;
-			case "Corn":
-				System.out.println("You chose corn from the Grains category.");
-				price=20;
-				System.out.println("Price is "+price);
-				break;
-			case "Barley":
-				System.out.println("You chose barley from the Grains category.");
-				price=30;
-				System.out.println("Price is "+price);
-				break;
-			case "Sorghum":
-				System.out.println("You chose sorghum from the Grains category.");
-				price=30;
-				System.out.println("Price is "+price);
-				break;
-			case "Millet":
-				System.out.println("You chose millet from the Grains category.");
-				price=25;
-				System.out.println("Price is "+price);
-				break;
-			default:
-				System.out.println("INVALID INPUT");
-				System.out.println("Enter the valid input");
-				itemInput5 = sc.nextLine();
-				break;
-			}
+			itemInput5();
 			break;
+
 		default:
 			System.out.println(" ");
 			System.out.println("Not Available");
-			System.out.println("Enter the valid category");
-			categoryInput = sc.nextLine();
+			System.out.println("Enter the valid category[Type exactly how it shows]");
+			categoryDetails();
 			break;
 		}
 	}
 
-	/*public void priceDetails() {
-		System.out.println(" ");
-		System.out.println("Enter the price");
+	private void itemInput5() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the item you want:");
 		Scanner sc = new Scanner(System.in);
-		price = sc.nextInt();
-	}*/
+		String itemInput5 = sc.nextLine();
+
+		switch (itemInput5) {
+		case "Wheat":
+			System.out.println("You chose wheat from the Grains category.");
+			price = 50;
+			System.out.println("Price is " + price);
+			break;
+		case "Oats":
+			System.out.println("You chose oats from the Grains category.");
+			price = 50;
+			System.out.println("Price is " + price);
+			break;
+		case "Rice":
+			System.out.println("You chose rice from the Grains category.");
+			price = 60;
+			System.out.println("Price is " + price);
+			break;
+		case "Corn":
+			System.out.println("You chose corn from the Grains category.");
+			price = 20;
+			System.out.println("Price is " + price);
+			break;
+		case "Barley":
+			System.out.println("You chose barley from the Grains category.");
+			price = 30;
+			System.out.println("Price is " + price);
+			break;
+		case "Sorghum":
+			System.out.println("You chose sorghum from the Grains category.");
+			price = 30;
+			System.out.println("Price is " + price);
+			break;
+		case "Millet":
+			System.out.println("You chose millet from the Grains category.");
+			price = 25;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput5();
+			break;
+		}
+	}
+
+	private void itemInput4() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the item you want:");
+		Scanner sc = new Scanner(System.in);
+		String itemInput4 = sc.nextLine();
+
+		switch (itemInput4) {
+		case "Coca-Cola":
+			System.out.println("You chose Coca-Cola from the Drinks category.");
+			price = 25;
+			System.out.println("Price is " + price);
+			break;
+		case "Pepsi":
+			System.out.println("You chose Pepsi from the Drinks category.");
+			price = 30;
+			System.out.println("Price is " + price);
+			break;
+		case "Limca":
+			System.out.println("You chose Limca from the Drinks category.");
+			price = 15;
+			System.out.println("Price is " + price);
+			break;
+		case "Sprite":
+			System.out.println("You chose Sprite from the Drinks category.");
+			price = 35;
+			System.out.println("Price is " + price);
+			break;
+		case "Fanta":
+			System.out.println("You chose Fanta from the Drinks category.");
+			price = 25;
+			System.out.println("Price is " + price);
+			break;
+		case "Appy Fizz":
+			System.out.println("You chose Appy Fizz from the Drinks category.");
+			price = 30;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput4();
+			break;
+		}
+	}
+
+	private void itemInput3() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the item you want:");
+		Scanner sc = new Scanner(System.in);
+		String itemInput3 = sc.nextLine();
+
+		switch (itemInput3) {
+		case "Notebooks":
+			System.out.println("You chose notebooks from the Stationery category.");
+			price = 50;
+			System.out.println("Price is " + price);
+			break;
+		case "Diaries":
+			System.out.println("You chose diaries from the Stationery category.");
+			price = 200;
+			System.out.println("Price is " + price);
+			break;
+		case "Pen":
+			System.out.println("You chose pen from the Stationery category.");
+			price = 20;
+			System.out.println("Price is " + price);
+			break;
+		case "Pencils":
+			System.out.println("You chose pencils from the Stationery category.");
+			price = 5;
+			System.out.println("Price is " + price);
+			break;
+		case "Calculator":
+			System.out.println("You chose calculator from the Stationery category.");
+			price = 500;
+			System.out.println("Price is " + price);
+			break;
+		case "Punching Machine":
+			System.out.println("You chose punching machine from the Stationery category.");
+			price = 600;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput3();
+			break;
+		}
+	}
+
+	private void itemInput2() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the item you want:");
+		Scanner sc = new Scanner(System.in);
+		String itemInput2 = sc.nextLine();
+
+		switch (itemInput2) {
+		case "Almonds":
+			System.out.println("You chose almonds from the Nuts category.");
+			price = 4;
+			System.out.println("Price is " + price);
+			break;
+		case "Walnuts":
+			System.out.println("You chose walnuts from the Nuts category.");
+			price = 5;
+			System.out.println("Price is " + price);
+			break;
+		case "Pecans":
+			System.out.println("You chose pecans from the Nuts category.");
+			price = 3;
+			System.out.println("Price is " + price);
+			break;
+		case "Cashews":
+			System.out.println("You chose cashews from the Nuts category.");
+			price = 8;
+			System.out.println("Price is " + price);
+			break;
+		case "Hazelnuts":
+			System.out.println("You chose hazelnut from the Nuts category.");
+			price = 10;
+			System.out.println("Price is " + price);
+			break;
+		case "Peanuts":
+			System.out.println("You chose peanuts from the Nuts category.");
+			price = 3;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput2();
+			break;
+		}
+	}
+
+	private void itemInput1() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the item you want:");
+		Scanner sc = new Scanner(System.in);
+		String itemInput1 = sc.nextLine();
+		itemInput1();
+		switch (itemInput1) {
+		case "Potato":
+			System.out.println("You chose potato from the Vegetables category.");
+			price = 10;
+			System.out.println("Price is " + price);
+			break;
+		case "Tomato":
+			System.out.println("You chose tomato from the Vegetables category.");
+			price = 5;
+			System.out.println("Price is " + price);
+			break;
+		case "Onion":
+			System.out.println("You chose onion from the Vegetables category.");
+			price = 6;
+			System.out.println("Price is " + price);
+			break;
+		case "Carrot":
+			System.out.println("You chose carrot from the Vegetables category.");
+			price = 8;
+			System.out.println("Price is " + price);
+			break;
+		case "Beetroot":
+			System.out.println("You chose beetroot from the Vegetables category.");
+			price = 10;
+			System.out.println("Price is " + price);
+			break;
+		case "Broccoli":
+			System.out.println("You chose broccoli from the Vegetables category.");
+			price = 12;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput1();
+			break;
+		}
+	}
+
+	private void itemInput() {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the item you want:");
+		String itemInput = sc.nextLine();
+		switch (itemInput) {
+		case "Apple":
+			System.out.println("You chose Apple from the Fruits category.");
+			price = 10;
+			System.out.println("Price is " + price);
+			break;
+		case "Orange":
+			System.out.println("You chose orange from the Fruits category.");
+			price = 12;
+			System.out.println("Price is " + price);
+			break;
+		case "Grapes":
+			System.out.println("You chose grapes from the Fruits category.");
+			price = 15;
+			System.out.println("Price is " + price);
+			break;
+		case "Banana":
+			System.out.println("You chose banana from the Fruits category.");
+			price = 6;
+			System.out.println("Price is " + price);
+			break;
+		case "Watermelon":
+			System.out.println("You chose Watermelon from the Fruits category.");
+			price = 30;
+			System.out.println("Price is " + price);
+			break;
+		case "PineApple":
+			System.out.println("You chose pineapple from the Fruits category.");
+			price = 40;
+			System.out.println("Price is " + price);
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+			System.out.println("Enter the valid input[Type exactly how it shows]");
+			itemInput();
+			break;
+		}
+	}
+
+	/*
+	 * public void priceDetails() { System.out.println(" ");
+	 * System.out.println("Enter the price"); Scanner sc = new Scanner(System.in);
+	 * price = sc.nextInt(); }
+	 */
 
 	public void quantityDetails() {
 		System.out.println(" ");
--- eclipse-workspace/Grocery/src/com/chainsys/grocery/TestApp.java
+++ eclipse-workspace/Grocery/src/com/chainsys/grocery/TestApp.java
@@ -3,14 +3,17 @@ package com.chainsys.grocery;
 public class TestApp {
 
 	public static void main(String[] args) {
-		System.out.println("              WELCOME TO THE GROCER'S CHOICE");
+		System.out.println("\t\t" + "              WELCOME TO THE GROCER'S CHOICE");
+		System.out.println(" ");
+		System.out.println("Address:3/98 Anna Nagar,Chennai" + "\t\t\t\t\t\t" + "Contact:9443576543");
+		System.out.println("Opposite to KFC" + "\t\t\t\t\t\t\t\t" + "GSTIN: 03AWBPP8756K592");
 		AppA app = new AppA();
-		//app.getDetails();
+		// app.getDetails();
 		app.categoryDetails();
-		//app.priceDetails();
+		// app.priceDetails();
 		app.quantityDetails();
 		app.discount();
 		System.out.println(" ");
-		System.out.println("              THANK YOU!! COME AGAIN");
+		System.out.println("\t\t" + "              THANK YOU!! COME AGAIN");
 	}
 }
